a66ca891f26b3666b4bb7de96df0d97e9819ef50
HEMANTH M <128402149+hemanth0525@users.noreply.github.com>
2024-11-04 03:29:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+    basePath: '/gitmorph',
+    publicRuntimeConfig: {
+        basePath: '/gitmorph',
+    },
 };
-
 export default nextConfig;
